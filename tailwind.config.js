ba7e394ba634bce3b3fd5a63ce123f584aab2b39
Mridul <mridularafin@gmail.com>
2022-08-07 08:56:52 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        body : ['PT Sans'],
+        body2 : ["Comfortaa"]
+      }
+    },
+  },
+  plugins: [],
+}
